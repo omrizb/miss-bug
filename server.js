aab1ec5bcbbefbd92b8f7aa0b01c0f921306890a
omribarouch <omri.barouch@safebreach.com>
2024-06-12 10:06:42 +0300

    Create basic backend services

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const PORT = 3030
 
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello'))
+app.get('/', (req, res) => res.send('Hello!'))
 app.listen(PORT, () => console.log(`Server is up. Listening port ${PORT}.`))
